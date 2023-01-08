b1912289b79be790417f3691a2d448a054a4ff3e
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-08 12:47:42 +0530

    code

--- Loan.java
+++ Loan.java
@@ -1,34 +1,52 @@
 import java.util.*;
 class Bank
 {
-    private int P;
-    private int T;
-    private int R;
-    private int S = 2;
+    private float pA;
+    private float tD;
+    private float sI[];
+    private int farmer;
+    private static int rI;
+    private float a[];
+    private float b[];
+    static{
+        rI=2;
+    }
 
-    void account()
+    void input()
     {
         System.out.println("Enter how many Farmer apply for loan : ");
         Scanner sn = new Scanner(System.in);
-        int farmer = sn.nextInt();
-        int a[] = new int[farmer];
-        int b[] = new int[farmer];
-        for(int i = 0;i<farmer;i++)
+        farmer = sn.nextInt();
+        a = new float[farmer];
+        b = new float[farmer];
+        sI = new float[farmer];
+         for(int i = 0;i<farmer;i++)
         {
-            System.out.println("Farmer-"+(i+1)+" how many money to loan : ");
-            int amo = sn.nextInt();
-            a[i] = amo;
-            System.out.println("Farmer-"+(i+1)+" how many time period : ");
-            int tim = sn.nextInt();
-            b[i] = tim;
+            System.out.println("Farmer-"+(i+1)+" kindly enter the principal amount : ");
+            pA = sn.nextFloat();
+            a[i] = pA;
+            System.out.println("Farmer-"+(i+1)+" kindlt mention time duration required : ");
+            tD = sn.nextFloat();
+            b[i] = tD;
         }
-        for(int i = 0;i<farmer;i++)
-        {
-            int simpint = (a[i] * b[i] * S)/100;
-            System.out.println("Farmer-"+(i+1)+" your simple interest is : " + simpint);
-        }
-        
     }
+    void process()
+    {
+         
+         for(int i = 0;i<farmer;i++)
+        {
+             sI[i] = (a[i] * b[i] * rI)/100;
+              System.out.println("Farmer-"+(i+1)+" your simple interest is : " + sI[i]);
+            
+        }
+
+    }
+   
+       
+   
+       
+        
+    
 
 }
 
@@ -41,8 +59,28 @@ class Loan
 {
     public static void main(String[] args)
     {
-        Bank bn = new Bank();
-        bn.account();
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter 1 for running this application and -1 to exit : ");
+        int choice = sn.nextInt();
+        while(choice == 1)
+        {
+             Bank bn = new Bank();
+             bn.input();
+             bn.process();
+             System.out.println("Enter 1 for using this application again or else -1 to exit :");
+             choice = sn.nextInt();
+            if(choice == 1)
+            {
+                 continue;
+            }
+            else{
+                break;
+            }
+
+
+        }
+        System.out.println("Thanks! and visit us again.");
+       
 
     }
 }
