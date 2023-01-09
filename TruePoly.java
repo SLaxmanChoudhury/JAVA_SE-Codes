272d4ab5ccea528f386f88385a19a7fbbd0072a3
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-10 01:22:34 +0530

    code

--- TruePoly.java
+++ TruePoly.java
@@ -15,13 +15,21 @@ class Student1 extends Student
     {
         System.out.println("My name is Kalia");
     }
+    void branch()
+    {
+        System.out.println("Your branch is CSE");
+    }
 }
 class Student2 extends Student
 {
     void regNo()
     {
         System.out.println("My regNo is 54");
+    }void branch()
+    {
+        System.out.println("Your branch is CSE");
     }
+
 
 }
 class Teacher
@@ -30,6 +38,8 @@ class Teacher
     {
         st.name();
         st.regNo();
+       // st.branch();
+       //((Student1) st).branch(); //down class casting
     }
 }
 
@@ -37,6 +47,7 @@ class TruePoly
 {
     public static void main(String[] args)
     {
+        System.out.println("This code is all about Loose Coupling :-");
         Teacher tch = new Teacher();
         Student1 s1 = new Student1();
         Student2 s2 = new Student2();
