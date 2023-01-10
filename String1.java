41eb82721cd20a2642f801c9278b973ec37e0996
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-10 15:06:28 +0530

    code

---
+++ String1.java
@@ -0,0 +1,13 @@
+class String1
+{
+    public static void main(String[] args)
+    {
+        String s1 = "Laxman";
+        String s2 = new String("Kalia");
+        System.out.println(s1 = s2);
+        System.out.println(s1);
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+
+    }
+}
