6e230c2f108e66d179b1852ea8131fd34e97a050
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-13 23:59:52 +0530

    code

---
+++ Has_A_Setter.java
@@ -0,0 +1,29 @@
+class Address
+{
+    void ads()
+    {
+        System.out.println("Laxman");
+    }
+}
+class Student
+{
+   
+    void name()
+    {
+        Address ad = new Address();
+    }
+}
+
+class Has_A_Setter
+{
+    public static void main(String[] args)
+   {
+       Student stu = new Student();
+       stu.name();
+      
+   }
+    
+}
+
+
+//day 5 to 6 hr;
