6af9842b9c50345d691b93ff5ec75479c65d2e60
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-18 15:32:38 +0530

    code

---
+++ CompileChech.java
@@ -0,0 +1,8 @@
+class CompileChech
+{
+    public static void main()//Java file will be compiled but cannot be executed by JVM.
+                            // Hence it must be required in the main method.
+    {
+        System.out.println("Hello");
+    }
+}
