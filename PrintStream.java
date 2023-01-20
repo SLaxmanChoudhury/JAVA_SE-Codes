be6fe90b3329a16f8212403119be3b5f700eeb43
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-20 23:41:04 +0530

    code

---
+++ PrintStream.java
@@ -0,0 +1,10 @@
+class PrintStream
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        //System - Class
+        //out -  static instance reference variable
+        //println - This method is under PrintStream class.
+    }
+}
