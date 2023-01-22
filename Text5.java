55900aa466e3e937c6f541c36f26d351882beaf7
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-22 23:48:02 +0530

    code

---
+++ Text5.java
@@ -0,0 +1,12 @@
+interface Text5
+{
+    // static
+    // {
+    //    System.out.println("Static block");
+    // }
+    public static void main(String[] args)
+    {
+
+      System.out.println("Static block");
+    }
+}
