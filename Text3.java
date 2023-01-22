73ef9091b9e837cf01009ffe3a0c4054b969d3b4
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-22 23:10:58 +0530

    code

---
+++ Text3.java
@@ -0,0 +1,10 @@
+class Text3
+{
+    public static void main(String [] args)
+    {
+        StringBuilder a1 = new StringBuilder("Kalia");
+        StringBuilder a2 = new StringBuilder("Kalia");
+        System.out.println(a1 == a2);
+         System.out.println(a1.equals(a2));
+    }
+}
