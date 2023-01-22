335d88e6ee73df6eed850a917dad44c0d5e2f6f3
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-22 23:17:09 +0530

    code

--- Text3.java
+++ Text3.java
@@ -5,6 +5,7 @@ class Text3
         StringBuilder a1 = new StringBuilder("Kalia");
         StringBuilder a2 = new StringBuilder("Kalia");
         System.out.println(a1 == a2);
-         System.out.println(a1.equals(a2));
+        System.out.println(a1.equals(a2));
+        System.out.println(a1.toString().equals(a2.toString()));
     }
 }
