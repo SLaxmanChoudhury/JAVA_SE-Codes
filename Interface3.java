4bbae0c4dd1af9d96309a707a792d71f4941156f
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-23 21:50:07 +0530

    code

--- Interface3.java
+++ Interface3.java
@@ -3,23 +3,30 @@ interface IDemo
     void service();
     void process();
 }
-class Demo1Impl implements IDemo
+abstract class Demo1Impl implements IDemo
 {
     @Override
     public void service()
     {
-        System.out.println("This Srevice is provide.");
+        System.out.println("This is Srevice block.");
     }
+    public abstract void process();
+   
+}
+class Demo1Imp2 extends Demo1Impl
+{
     public void process()
     {
         System.out.println("This is process block");
+
     }
+
 }
-public class Interface2
+public class Interface3
 {
     public static void main(String[] args)
     {
-        IDemo demo = new Demo1Impl();
+        IDemo demo = new Demo1Imp2();
         demo.service();
         demo.process();
     }
