783e3acab63731629e596fa5c31324adce74ce1b
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-25 22:23:33 +0530

    code

---
+++ ValueOff.java
@@ -0,0 +1,13 @@
+class ValueOff
+{
+
+    public static void main(String[] args)
+    {
+        Integer in = new Integer(10);
+        System.out.println(in);
+
+        Integer in1 = Integer.valurOf(10);
+        System.out.println(in1);
+
+    }
+}
