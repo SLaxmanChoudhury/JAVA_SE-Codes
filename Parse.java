4aeb7ad673e3aea28afa838ce15a6eab51edb420
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-26 10:15:52 +0530

    code

---
+++ Parse.java
@@ -0,0 +1,14 @@
+class Parse
+{
+    public static void main(String []args)
+    {
+        int i = Integer.parseInt("10");
+        System.out.println(i);
+
+        boolean b = Boolean.parseBoolean("hjd");
+        System.out.println(b);
+
+        double d = Double.parseDouble("1001.23");
+        System.out.println(d);
+    }
+}
--- PtoW.java
+++ PtoW.java
@@ -10,6 +10,14 @@ class PtoW
         System.out.println(in.longValue());
         System.out.println(in.floatValue());
         System.out.println(in.doubleValue());
+
+        Character c =Character.valueOf('a');
+        char c1 = c.charValue();
+        System.out.println(c1);
+
+        Boolean b = Boolean.valueOf("jhdj");
+        boolean b1 = b.booleanValue();
+        System.out.println(b1);
     }
 
 }
