b32511f0d6d0fd3df9372a89376cd92aa42544d8
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-26 09:49:39 +0530

    code

---
+++ PtoW.java
@@ -0,0 +1,15 @@
+class PtoW
+{
+    public static void main(String[] args)
+    {
+        // Integer in = new Integer(150);
+        Integer in = Integer.valueOf(150);
+        System.out.println(in.byteValue());
+        System.out.println(in.shortValue());
+        System.out.println(in.intValue());
+        System.out.println(in.longValue());
+        System.out.println(in.floatValue());
+        System.out.println(in.doubleValue());
+    }
+
+}
