8fed46f63bd6b91fb061c7f1ff933233cd19a4f9
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-01-31 09:18:20 +0530

    code

---
+++ EH2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class EH2
+{
+    public static void main(String[] args)
+    {
+        Scanner sn = new Scanner(System.in);
+        try
+        {
+             System.out.println("Enter a Number : ");
+             int a = sn.nextInt();
+             System.out.println("Enter the 2nd NUmber : ");
+             int b = sn.nextInt();
+             int result = a/b;
+             System.out.println("The Results of devision is : " +result);
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("You try to devide by the number in zero(0) .");
+        }
+        System.out.println("Your calculation is complited. ");
+      
+    }
+}
