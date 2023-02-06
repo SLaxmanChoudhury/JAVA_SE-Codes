341982348ac00b398b394a37a804d8cfadb97c34
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-02-06 19:32:26 +0530

    code

---
+++ EH5Thred.java
@@ -0,0 +1,9 @@
+class EH5Thred
+{
+    public static void main(String []args)throws Exception
+    {
+        System.out.println("Before Sleepn");
+        Thread.sleep(5000);
+        System.out.println("After the nap");
+    }
+}
