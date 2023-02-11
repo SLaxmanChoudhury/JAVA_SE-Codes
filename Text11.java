8088827e4e84d2ec5e60a6496950ceb9ecb02863
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-02-11 23:41:04 +0530

    code

---
+++ Text11.java
@@ -0,0 +1,41 @@
+interface Demo11
+{
+    void demo11();
+    void kcc();
+}
+class Demo12 implements Demo11
+{
+    void demo11()
+    {
+
+    }
+    void kcc()
+    {
+
+    }
+
+}
+class Demo13 extends Demo12
+{
+     void demo11()
+     {
+        System.out.println("Panda");
+     }
+}
+class Demo14 extends Demo13
+{
+    void kcc()
+    {
+        System.out.println("Subham");
+    }
+}
+
+public class Text11
+{
+    public static void main(String... args)
+    {
+        Demo14 d14 = new Demo14();
+        d14.demo11();
+        d14.kcc();
+    }
+}
