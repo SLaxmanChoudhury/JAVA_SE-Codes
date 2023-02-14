13cab18c3f3991ef155be8341946055d8e6a6471
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-02-14 22:24:11 +0530

    code

---
+++ SOPln.java
@@ -0,0 +1,27 @@
+import java.io.PrintStream;
+// class Object
+// {
+
+// }
+
+// class PrintStream{
+//     void println(){
+
+//     }
+// }
+
+// class System
+// {
+//     static  PrintStream out = new PrintStream();
+
+
+// }
+class SOPln
+{
+    public static void main(String... args)
+    {
+        PrintStream ps = System.out;
+
+        ps.println("Hello World...");
+    }
+}
