48aff642171499cb74c329752e5a6a86fddd3bef
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-02-18 18:42:09 +0530

    code

---
+++ Ex2.java
@@ -0,0 +1,10 @@
+class Ex2
+{
+    public static void main(String... args)
+    {
+        String a = "";
+        System.out.println(a.isEmpty());
+        String b= " ";
+        System.out.println(b.isEmpty());
+    }
+}
