de040fd34bb867abae5d9ca61f2417b5aca4e1d2
SLaxmanChoudhury <slaxmanchoudhury@gmail.com>
2023-02-18 18:50:18 +0530

    code

---
+++ Ex3.java
@@ -0,0 +1,17 @@
+class If1
+{
+    public static void iff()
+    {
+        System.out.println("Hello KC");
+    }
+}
+
+interface Ex3
+{
+    public static void main(String... args)
+    {
+        //If1 i = new If1();
+        If1.iff();
+        System.out.println("Hello KC");
+    }
+}
